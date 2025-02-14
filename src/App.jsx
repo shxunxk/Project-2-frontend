e9e33cbd645653df3aceab9969029159bc59b9a5
Shaunak <ssnagvenkar@gmail.com>
2025-02-14 15:39:28 +0530

    Inital design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
 import Home from './Pages/Home';
 import { NavbarSimple } from './Components/Navbar';
+import Dataset from './Pages/Dataset';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <div className="w-12 h-screen text-white shadow-sm">
           <Sidebar />
         </div>
-        <div className="flex-1 p-5 overflow-auto">
+        <div className="flex-1 px-3 overflow-auto">
           <div>
             <NavbarSimple/>
           </div>
@@ -19,6 +20,7 @@ function App() {
           <div >
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/:id" element={<Dataset />} />
             </Routes>
           </div>
         </div>
--- src/Pages/Dataset.jsx
+++ src/Pages/Dataset.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Dataset() {
   return (
-    <div className="h-full w-full">
-      <h3>Dataset title</h3>
+    <div className="h-full w-full p-5 my-10">
+      <h3 className='text-4xl font-bold'>Dataset title</h3>
       <p>Other details</p>
     </div>
   )
